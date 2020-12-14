b7c68ba1052be8952895033cc0a521ab498ea726
Andrii Kulba <avk.0149344@gmail.com>
2020-12-14 21:11:36 +0200

    14.12 update

--- index3.js
+++ index3.js
@@ -11,14 +11,15 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        const data = { id: transaction.id, time: delay };
+        resolve(data);
       }
       reject(transaction.id);
     }, delay);
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
